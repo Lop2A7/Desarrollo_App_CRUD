0797ae07418b5f9851c4aa7889f09c6742e985a4
Edson <edson.bejar@ucsm.edu.pe>
2023-12-08 15:35:20 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import { useState } from "react";
+import Axios from "axios";
+
+function App() {
+  const [nombre, setNombre] = useState("");
+  const [edad, setEdad] = useState(0);
+  const [pais, setPais] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [anios, setAnios] = useState(0);
+
+  const add = () => {
+    Axios.post("http://localhost:3001/create", {
+      nombre: nombre,
+      edad: edad,
+      pais: pais,
+      cargo: cargo,
+      anios: anios
+    }).then(() => {
+      alert("Empleado registrado");
+    }).catch((error) => {
+      console.error("Error al registrar el empleado", error);
+    });
+  };
+  return (
+    <div className="App">
+      <div className="datos">
+        <label>Nombre: <input 
+        onChange={(event) => {
+          setNombre(event.target.value);
+        }}  
+        type="text"/></label>
+        <label>Edad:<input 
+        onChange={(event) => {
+          setEdad(event.target.value);
+        }}  
+        type = "number"/></label>
+        <label>Pais:<input
+        onChange={(event) => {
+          setPais(event.target.value);
+        }} type = "text"/></label>
+        <label>Cargo:<input
+        onChange={(event) => {
+          setCargo(event.target.value);
+        }} type = "text"/></label>
+        <label>Años:<input
+        onChange={(event) => {
+          setAnios(event.target.value);
+        }} type = "number"/></label>
+        <button onClick={add}>Registrar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
